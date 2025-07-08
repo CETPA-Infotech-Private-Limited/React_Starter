49a8e7e765b1e6512bf181071ccf0a37ff199478
ranjeet-cetpa <ranjeet@cetpainfotech.com>
2025-07-08 11:57:58 +0530

    latest code

--- src/components/user/RequestAdvanceTable.tsx
+++ src/components/user/RequestAdvanceTable.tsx
@@ -1,7 +1,21 @@
+import { Plus } from 'lucide-react';
+import { Button } from '../ui/button';
 import TableList from '../ui/data-table';
 
-const RequestAdvanceTable = ({ columns, data }) => {
-  return <TableList columns={columns} data={data} showFilter={true} showSearchInput />;
+const RequestAdvanceTable = ({ columns, data, onAddClick }) => {
+  return (
+    <TableList
+      columns={columns}
+      data={data}
+      showFilter={true}
+      showSearchInput
+      rightElements={
+        <Button onClick={onAddClick}>
+          <Plus /> New Advance Claim Request
+        </Button>
+      }
+    />
+  );
 };
 
 export default RequestAdvanceTable;
