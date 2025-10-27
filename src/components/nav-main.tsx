febd4fc03e75e9b1589d1be485b446f8a0989fd3
Abhishek Nautiyal <abhishek2@cetpainfotech.com>
2025-10-27 12:42:11 +0530

    sidebar changes

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -9,7 +9,6 @@ export function NavMain({
     title: string;
     url: string;
     icon?: LucideIcon;
-    roles: any;
   }[];
 }) {
   const { setOpenMobile } = useSidebar();
@@ -29,7 +28,9 @@ export function NavMain({
                 >
                   <div
                     className={`flex items-center gap-2 ${
-                      isActive ? 'bg-primary text-white hover:text-white h-full w-full' : 'hover:bg-primary hover:text-white active:text-white  h-full'
+                      isActive
+                        ? 'bg-primary text-white hover:text-white h-full w-full'
+                        : 'hover:bg-primary hover:text-white active:text-white  h-full'
                     }`}
                   >
                     {item.icon && <item.icon size={24} />}
