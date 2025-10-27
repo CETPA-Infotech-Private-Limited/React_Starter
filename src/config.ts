aa79474ef894ef0ba7e34d1e5750e8a996432276
Abhishek Nautiyal <abhishek2@cetpainfotech.com>
2025-10-27 14:40:45 +0530

    config file change

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const DFCCIL_UAT = {
-  apiUrl: 'https://uat.guesthouse.cetpainfotech.com/api',
+  apiUrl: 'https://uattourapi.dfccil.com/api',
   orgHierarchy: 'https://uatorganization.dfccil.com/api',
   logoutUrl: 'http://uat.dfccil.com/DfcHome',
   exitUrl: 'http://uatlogin.dfccil.com/applications',
