05a5d23b06c2daa2b6b8441ee86f206640efff5f
ranjeet-cetpa <ranjeet@cetpainfotech.com>
2026-03-16 12:43:24 +0530

    lastest code

--- src/hooks/useAppName.ts
+++ src/hooks/useAppName.ts
@@ -4,9 +4,9 @@ import { useSelector } from 'react-redux';
 export const useAppName = () => {
   const applications = useSelector((state: any) => state.applications.applications);
   const currentApplication = applications.find((app) => app.id === environment.applicationId) ?? {
-    name: 'App Name',
-    hindiName: 'e-Name',
-    description: 'App Name',
+    name: '',
+    hindiName: '',
+    description: '',
   };
 
   return {
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -15,9 +15,6 @@ import AppLayout from '@/components/layout/app-layout';
 import AdminManagement from '@/pages/admin/AdminManagement';
 import Seo from '@/components/common/Seo';
 import { useAppName } from '@/hooks/useAppName';
-import RaiseClaim from '@/pages/RaiseClaim/RaiseClaim';
-import ApprovarPage from '@/pages/RaiseClaim/ApproverPage';
-import ApproverPage from '@/pages/RaiseClaim/ApproverPage';
 
 const AppRoutes = () => {
   const dispatch = useAppDispatch();
@@ -41,9 +38,6 @@ const AppRoutes = () => {
           <Route element={<PrivateRoute allowedRoles={['user', 'HR Admin']} />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path='/raise-claim' element={<RaiseClaim/>} />
-            <Route path='/raise-claim-approver' element={<ApproverPage/>} />
-
           </Route>
         </Route>
 
